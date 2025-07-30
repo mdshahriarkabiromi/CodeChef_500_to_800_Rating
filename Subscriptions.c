8b71f2b15d8600c462b619c99f8b75e54fae3e94
mdshahriarkabiromi <shahriar.eu.cse59@gmail.com>
2025-07-30 08:13:01 +0600

    https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SUBSCRIBE_

--- Subscriptions.c
+++ Subscriptions.c
@@ -1,30 +1,21 @@
-
 #include<stdio.h>
-int main()
-{
-    int T;
-    scanf("%d",&T);
-    for(int i=1;i<=T;i++)
-    {
-        int N,X,cost;
-        scanf("%d %d ",&N,&X);
-        if(N>6)
-        {
-            cost=N/6;
-            if(N%6 != 0)
-            {
-                cost++;
-            }
-            cost=(cost*X);
-        }
-        else
-        {
-            cost=X/N;
-        }
-        printf("%d\n",cost);
+#include<math.h>
 
+int main() {
+    int T;
+    scanf("%d", &T);
+    for(int i = 1; i <= T; i++) {
+        int N, X, cost;
+        scanf("%d %d", &N, &X);
         
+        if (N > 6) {
+            int subscription_number = (int)ceil((float)N / 6);
+            cost = X * subscription_number;
+        } else {
+            cost = X;
+        }
+        
+        printf("%d\n", cost);
     }
-    
     return 0;
 }
