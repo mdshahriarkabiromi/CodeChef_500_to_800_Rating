8feee7fc6b967bb1e7bf209c92bac47179a18918
mdshahriarkabiromi <shahriar.eu.cse59@gmail.com>
2025-08-01 08:03:25 +0600

    https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SUGARCANE?tab=statement

---
+++ Sugarcane_Juice_Business.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int T;
+    scanf("%d",&T);
+    for (int i = 0; i < T; i++)
+    {
+        int N;
+        scanf("%d",&N);
+        printf("%d\n", N*15);
+
+    }
+    
+
+    return 0;
+}
