1ce848643a51362ad6969c98a73c91de80bd43e6
mdshahriarkabiromi <shahriar.eu.cse59@gmail.com>
2025-08-01 21:33:45 +0600

    https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/TRAVELFAST

---
+++ Car_or_Bike.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int T;
+    scanf("%d",&T);
+    for (int i = 0; i < T; i++)
+    {
+        int X,Y;
+        scanf("%d %d",&X,&Y);
+        if (X<Y)
+        {
+            printf("BIKE\n");
+        }
+        else if (X>Y)
+        {
+            printf("CAR\n");
+        }
+        
+        else
+        {
+            printf("SAME\n");
+        }
+        
+    }
+    return 0;
+}
